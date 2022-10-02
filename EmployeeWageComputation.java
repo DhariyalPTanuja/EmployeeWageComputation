78bf5f4d1f517520226a1904a543f959b778c466
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-10-02 16:56:47 +0530

    Refactor the Code

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -17,55 +17,19 @@ public class EmployeeWageComputation {
 //- Assume 100 hours and 20 days
 
     public EmployeeWageComputation() {
-        System.out.println(" Employee Wage Computation Program UC6  !!!");
-        System.out.println("  Calculate Wages till a condition of total working hours or days is reached for a month");
+        System.out.println(" Employee Wage Computation Program UC7  !!!");
+        System.out.println("  Refactor the Code to write a Class Method to Compute Employee Wage");
 
     }
 
     public static void main(String[] args) {
         System.out.println("welcome to the Employee Wage Computation!!!");
         EmployeeWageComputation empobj = new EmployeeWageComputation();
-        empobj.calculateMonthlyWage();
+        EmployeeWageFunctions empFunObj = new EmployeeWageFunctions();
+        empFunObj.calculateMonthlyWage();
 
 
     }
 
-    public int getRandomNumber() {
-        int isPresent = (int) (Math.floor(Math.random() * 10) % 3);
-        return isPresent;
-    }
-
-    public void checkEmployee() {
-        isPresent = getRandomNumber();
-
-        //System.out.println(isPresent);
-        switch (isPresent) {
-            case 0:
-                System.out.println("Employee is absent");
-                workingHour = 0;
-                break;
-            case 1:
-                System.out.println("Employee is Present");
-                workingHour = 8;
-                break;
-            default:
-                System.out.println("Employee is working  Part-Time");
-                workingHour = 8;
-                break;
-        }
-    }
-    // Calculate Employee Wage
-    public void calculateMonthlyWage() {
-            while (totalWorkingHour < hoursInMonth && totalworkingDays < numWorkingDays) {
-                checkEmployee();
-                int dailyWage = wagePerHour * workingHour;
-                totalWorkingHour = totalWorkingHour + workingHour;
-                totalSalary = dailyWage + totalSalary;
-                totalworkingDays++;
-            }
-            System.out.println("totalworkingDays = " + totalworkingDays);
-        System.out.println("totalworkingHours = " + totalWorkingHour);
-            System.out.println("the total wage of employee is = " + totalSalary);
-        }
 
 }
